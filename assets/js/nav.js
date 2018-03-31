42224662c7e6c54a8d3d90d893e649a1b3f49e23
Flo <x.client.ich@gmail.com>
2018-03-31 19:59:23 +0200

    not used shit

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,9 +1,7 @@
 function openNav() {
   document.getElementById('nav').style.transform = 'translateY(0%)'
-  updateURL('#open')
 }
 
 function closeNav() {
   document.getElementById('nav').style.transform = 'translateY(-100%)'
-  updateURL('')
 }
